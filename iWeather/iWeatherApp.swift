72e87f6e52d37523817bed2b4c8318d8ca524a0f
Bonmyeong Koo - vn56eqd <Bonmyeong.Koo@walmart.com>
2025-05-29 17:45:14 -0400

    Initial Commit

---
+++ iWeather/iWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  iWeatherApp.swift
+//  iWeather
+//
+//  Created by Bonmyeong Koo - Vendor on 5/29/25.
+//
+
+import SwiftUI
+
+@main
+struct iWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iWeatherUITests/iWeatherUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  iWeatherUITestsLaunchTests.swift
+//  iWeatherUITests
+//
+//  Created by Bonmyeong Koo - Vendor on 5/29/25.
+//
+
+import XCTest
+
+final class iWeatherUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
