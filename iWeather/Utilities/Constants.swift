5533a3a1ae85594001c365e99c1ae744e86b92d0
Bonmyeong Koo - vn56eqd <Bonmyeong.Koo@walmart.com>
2025-05-29 17:53:19 -0400

    Set up the base project

---
+++ iWeather/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  iWeather
+//
+//  Created by Bonmyeong Koo - Vendor on 5/29/25.
+//
+
+import Foundation
+
+enum Constants {
+    static let apiKey = "YOUR_API_KEY"
+    static let baseURL = "https://api.weatherapi.com/v1/"
+}
